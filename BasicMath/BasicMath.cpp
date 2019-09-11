c5668605a56b6591c9c20ca2ce08f5da28248e4c
Unknown <sam@malpass.me.uk>
2019-09-11 17:09:02 +0100

    Create the GeometryFuncs class

--- BasicMath/BasicMath.cpp
+++ BasicMath/BasicMath.cpp
@@ -45,3 +45,43 @@ std::vector<double> AlgebraFuncs::QuadraticFormula(double a, double b, double c)
 	data.push_back(x2);
 	return data;
 }
+
+/*
+	Functions for the GeometryFuncs class
+*/
+double GeometryFuncs::SquareArea(double a)
+{
+	return a * a;
+}
+double GeometryFuncs::RectangleArea(double a, double b)
+{
+	return a * b;
+}
+double GeometryFuncs::TriangleArea(double a, double b)
+{
+	return ((a * b) / 2);
+}
+double GeometryFuncs::RhombusArea(double a, double b)
+{
+	return ((a * b) / 2);
+}
+double GeometryFuncs::TrapezoidArea(double a, double b, double c)
+{
+	return (((a + b) / 2) * c);
+}
+double GeometryFuncs::PolygonArea(double a, double b)
+{
+	return ((a / 2) * b);
+}
+double GeometryFuncs::CircleArea(double a)
+{
+	return (PI * AdvFuncs::Power(a, 2));
+}
+double GeometryFuncs::ConeArea(double a, double b)
+{
+	return ((PI * a) * b);
+}
+double GeometryFuncs::SphereArea(double a)
+{
+	return (4 * (PI * AdvFuncs::Power(a, 2)));
+}
--- BasicMath/BasicMath.h
+++ BasicMath/BasicMath.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#define PI 3.14159265359
 /* BasicMath.h by Samuel John Malpass*/
 
 namespace BasicMath
@@ -32,5 +33,36 @@ namespace BasicMath
 	public:
 		//Function declaration for Quadratic Formula
 		static std::vector<double> QuadraticFormula(double a, double b, double c);
+	};
+
+	class GeometryFuncs
+	{
+	public:
+		//Function declaration for SquareArea
+		static double SquareArea(double a);
+
+		//Function declaration for RectangleArea
+		static double RectangleArea(double a, double b);
+
+		//Function declaration for TriangleArea
+		static double TriangleArea(double a, double b);
+
+		//Function declaration for RhombusArea
+		static double RhombusArea(double a, double b);
+
+		//Function declaration for TrapezoidArea
+		static double TrapezoidArea(double a, double b, double c);
+
+		//Function declaration for PolygonArea
+		static double PolygonArea(double a, double b);
+
+		//Function declaration for CircleArea
+		static double CircleArea(double a);
+
+		//Function declaration for ConeArea
+		static double ConeArea(double a, double b);
+
+		//Function declaration for SphereArea
+		static double SphereArea(double a);
 	};
 }
