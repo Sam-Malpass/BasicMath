7405bd813463a02664a69fa696ff771dcf093f7b
Unknown <sam@malpass.me.uk>
2019-09-11 15:36:05 +0100

    Create and implement CoreFuncs class

--- BasicMath/BasicMath.h
+++ BasicMath/BasicMath.h
@@ -3,5 +3,19 @@
 
 namespace BasicMath
 {
+	class CoreFuncs
+	{
+	public:
+		//Function declaration for Add
+		static double Add(double a, double b);
+		
+		//Function declaration for Subtract
+		static double Subtract(double a, double b);
 
+		//Function declaration for Multiply
+		static double Multiply(double a, double b);
+
+		//Function declaration for Divide
+		static double Divide(double a, double b);
+	};
 }
